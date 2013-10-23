3149c47333269057e0a202b6f48604b9fbdd2c3b
Craig Boileau <craigboileau@gmail.com>
2013-10-23 14:31:17 -0400

    updated SDK and examples.php docs

--- SocialAdrAPI.php
+++ SocialAdrAPI.php
@@ -42,6 +42,18 @@ class SocialAdrAPI {
 	public function setAccessToken($token) {
 		$this->accessToken = $token;
 	}
+        public function setSubaccountGUID($guid){
+                $this->subaccountGUID=$guid;
+        }
+        public function addSubaccountData($postData){
+            if(!empty($this->subaccountGUID)){
+                if(empty($postData)){
+                    $postData = array();
+                }
+                $postData['subaccount_guid'] = $this->subaccountGUID;
+            }
+            return $postData;
+        }
 	public function handleAuth() {
 		$code = $_GET ['code'];
 		if (empty($code)) {
@@ -99,6 +111,23 @@ class SocialAdrAPI {
 		$result = $this->_postRequest($endpoint, $postData);
 		return $result;
 	}
+        public function fblikesHistory($limit=100, $offset=0){
+            $this->requireAccessToken();
+            $endpoint = $this->api . '/oauth/fblikes/history?access_token=' . $this->accessToken;
+            $postData = array (
+				'limit' => $limit,
+				'offset' => $offset
+            );
+            $result = $this->_postRequest($endpoint, $postData);
+            return $result;
+        }
+        public function resellerSubaccounts(){
+            $this->requireAccessToken();
+            $endpoint = $this->api . '/oauth/reseller/subaccounts?access_token=' . $this->accessToken;
+            $result = $this->_getRequest($endpoint);
+            return $result;
+        }
+        
 	public function urlValidate($url) {
 		$this->requireAccessToken();
 		$postData = array (
@@ -108,6 +137,24 @@ class SocialAdrAPI {
 		$result = $this->_postRequest($endpoint, $postData);
 		return $result;
 	}
+        public function urlDelete($bookmark_id){
+		$this->requireAccessToken();
+		$postData = array (
+				'bookmark_id' => $bookmark_id
+		);
+		$endpoint = $this->api . '/oauth/url/delete?access_token=' . $this->accessToken;
+		$result = $this->_postRequest($endpoint, $postData);
+		return $result;
+        }
+        public function urlUndelete($bookmark_id){
+		$this->requireAccessToken();
+		$postData = array (
+				'bookmark_id' => $bookmark_id
+		);
+		$endpoint = $this->api . '/oauth/url/undelete?access_token=' . $this->accessToken;
+		$result = $this->_postRequest($endpoint, $postData);
+		return $result;
+        }
 	public function urlAdd(SocialAdrURL $url) {
 		$this->requireAccessToken();
 		$postData = array (
@@ -124,6 +171,23 @@ class SocialAdrAPI {
 		$result = $this->_postRequest($endpoint, $postData);
 		return $result;
 	}
+        public function urlUpdate(SocialAdrURL $url) {
+		$this->requireAccessToken();
+		$postData = array (
+                                'guid' => $url->guid,
+				'url' => $url->url,
+				'title' => $url->title,
+				'descr' => $url->descr,
+				'tags' => $url->tags,
+				'category' => $url->category,
+				'microblog' => $url->microblog,
+				'submitRate' => $url->submitRate,
+				'submitLimit' => $url->submitLimit 
+		);
+		$endpoint = $this->api . '/oauth/url/update?access_token=' . $this->accessToken;
+		$result = $this->_postRequest($endpoint, $postData);
+		return $result;
+	}
 	public function urlList($limit = 100, $offset = 0, $sort = 'guid', $sort_direction = 'asc') {
 		$endpoint = $this->api . '/oauth/url/list?access_token=' . $this->accessToken;
 		$postData = array (
@@ -135,7 +199,18 @@ class SocialAdrAPI {
 		$result = $this->_postRequest($endpoint, $postData);
 		return $result;
 	}
-	public function reportDetail($url, $offset = 0, $limit = 10) {
+        public function urlArchived($limit = 100, $offset = 0, $sort = 'guid', $sort_direction = 'asc'){
+		$endpoint = $this->api . '/oauth/url/archived?access_token=' . $this->accessToken;
+		$postData = array (
+				'limit' => $limit,
+				'offset' => $offset,
+				'sort' => $sort,
+				'sort_direction' => $sort_direction 
+		);
+		$result = $this->_postRequest($endpoint, $postData);
+		return $result;
+        }
+	public function reportDetail($url, $offset = 0, $limit = 50) {
 		$this->requireAccessToken();
 		$postData = array (
 				'url' => $url,
@@ -167,13 +242,12 @@ class SocialAdrAPI {
 	public function throwError($level, $error) {
 		$this->Error->logError($level, $error);
 	}
-
 	public function errors($responseType = 'html') {
 		return $this->Error->outputLog($responseType);
 	}
 	private function _apiCall($rType = 'GET', $endpoint, $data = null) {
 		$ch = curl_init();
-		$this->_curlOptions($ch, $rType, $endpoint, $data);
+		$data = $this->_curlOptions($ch, $rType, $endpoint, $data);
 		$curlResponse = curl_exec($ch);
 		$this->lastError = curl_error($ch);
 		$this->curlInfo = curl_getinfo($ch);
@@ -183,10 +257,10 @@ class SocialAdrAPI {
 			echo '<li>Sending ' . $rType . ' Request to ' . $endpoint . '</li>';
 			echo 'Request Data:';
 			echo '<pre>' . print_r($data, true) . '</pre>';
-			echo 'Response JSON:';
+			echo 'Response:';
 			echo '<pre>' . $curlResponse . '</pre>';
 			echo 'Response JSON:';
-			echo '<pre>' . print_r(json_decode($curlResponse)) . '</pre>';
+			echo '<pre>' . print_r(json_decode($curlResponse), true) . '</pre>';
 			echo 'CURL Errors:';
 			echo '<pre>' . $this->lastError . '</pre>';
 			echo '<pre>' . print_r($this->curlInfo) . '</pre>';
@@ -195,6 +269,7 @@ class SocialAdrAPI {
 		return json_decode($curlResponse);
 	}
 	private function _curlOptions($ch, $rType, $endpoint, $data) {
+                $data = $this->addSubaccountData($data);
 		curl_setopt_array($ch, array (
 				CURLOPT_URL => $endpoint,
 				CURLOPT_HEADER => false,
@@ -206,6 +281,7 @@ class SocialAdrAPI {
 				CURLOPT_TIMEOUT => 20,
 				CURLOPT_PORT => 80 
 		));
+                return $data;
 	}
 	private function _putRequest($endpoint, $data = null) {
 		return $this->_apiCall('PUT', $endpoint, $data);
--- examples.php
+++ examples.php
@@ -3,7 +3,7 @@
 /* 
 	Some examples usages of the various API methods
 	
-	The API methods are described in https://docs.google.com/document/d/1Sai2VVAYCcMilB02EzpoTcBbpb0YSNxW_YbCZeJrHlk/edit?usp=sharing
+	The API methods are described in http://socialadr.dev/pg/apps/api-methods
 	
 	To find your Client ID, Client Secret, and App ID, go to http://socialadr.com/pg/apps/manage and click "Manage" for your app
 */
@@ -21,16 +21,19 @@
 
 	// Validate method
 	// Find out if it's possible to add a URL into the system, before attempting to do so.
+        // $SocialAdr->urlValidate($url)
 	$result = $SocialAdr->urlValidate('http://somesite.com/somepage');	
 	print_r($result);
 	
 	// List method
 	// Return a list of bookmarks in an account
+        // $SocialAdr->urlList($limit)
 	$result = $SocialAdr->urlList(10);
 	print_r($result);
 	
 	// urlAdd method
 	// Add a new URL that you want to promote.
+        // $SocialAdr->urlAdd(SocialAdrURL $url)
 	$myURL = new SocialAdrURL;
 	$myURL->url = 'http://somesite.com/somepage';
 	$myURL->title = 'A {great|awesome|cool|fun} webpage';
@@ -43,25 +46,80 @@
 	$result = $SocialAdr->urlAdd($myURL);
 	print_r($result);	
 	
+	// urlUpdate method
+	// Updates an existing URL.
+        // $SocialAdr->urlUpdate(SocialAdrURL $url)
+	$myURL = new SocialAdrURL;
+        $myURL->guid = 12345;
+	$myURL->url = 'http://somesite.com/somedifferentpage';
+	$myURL->title = 'A new {great|awesome|cool|fun} webpage';
+	$myURL->descr = 'This is now the {best|most amazing} {webpage|website} I think I have ever {viewed|looked at|seen} in my entire life! {Highly|Definitely} recommend.';
+	$myURL->tags = 'here,are,some,new tags';
+	$myURL->category = 'inte';
+	$myURL->microblog = 'A {great|awesome|cool|fun} updated webpage';
+	$myURL->submitRate = 'fast';
+	$myURL->submitLimit = 200;
+	$result = $SocialAdr->urlUpdate($myURL);
+	print_r($result);	
+        
+        // urlDelete method
+	// Deletes URL with given guid
+        // $SocialAdr->urlDelete($guid)
+	$result = $SocialAdr->urlDelete(12345);
+	print_r($result);
+        
+        // urlUndelete method
+	// Restores deleted URL with given guid
+        // $SocialAdr->urlUndelete($guid)
+	$result = $SocialAdr->urlUndelete(12345);
+	print_r($result);
+        
+        // urlList method
+	// Lists URLs
+        // $SocialAdr->urlList($limit, $offset, $sort, $sort_direction)
+	$result = $SocialAdr->urlList(15, 0, 'guid', 'asc');
+	print_r($result);
+        
+        // urlArchived method
+	// Lists archived URLs
+        // $SocialAdr->urlArchived($limit, $offset, $sort, $sort_direction)
+	$result = $SocialAdr->urlArchived(15, 0, 'guid', 'asc');
+	print_r($result);
+        
 	// reportOverview method
 	// Gives an overview of the latest social submissions for all URLs in an account.
-	$result = $SocialAdr->reportOverview(0,20);
+        // $SocialAdr->reportOverview($limit,$offset)
+	$result = $SocialAdr->reportOverview(20,0);
 	print_r($result);	
 	
 	// reportDetail method
 	// Gives detailed history of social submissions for a specific URL
-	$result = $SocialAdr->reportDetail('http://somesite.com/somepage',0,20);
+        // $SocialAdr->reportDetail($url,$limit,$offset)
+	$result = $SocialAdr->reportDetail('http://somesite.com/somepage',20,0);
 	print_r($result);
 	
-	//fblikesAdd method
+	// fblikesAdd method
 	// Creates a Facebook Likes Campaign with given package for given bookmark
+        // $SocialAdr->fblikesAdd($url_guid, $package_id)
 	$result = $SocialAdr->fblikesAdd('794101', 1);
 	print_r($result);
 	
-	//fblikesPackages method
+	// fblikesPackages method
 	// Lists all available packages for Facebook Likes Campaigns, including number of likes, and credits required
 	$result = $SocialAdr->fblikesPackages();
 	print_r($results);
+        
+        // fblikesHistory method
+	// List Facebook Likes campaigns
+        // $SocialAdr->fblikesHistory($limit, $offset)
+	$result = $SocialAdr->fblikesHistory(10, 0);
+	print_r($results);
 	
-
+        // resellerSubaccounts method
+        // Lists all subaccounts controlled by reseller
+        // $SocialAdr->resellerSubaccounts()
+        $results = $SocialAdr->resellerSubaccounts();
+        print_r($results);
+        
+        
 ?>
