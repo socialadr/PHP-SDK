f13e09a9719a496457861383e7233ddc4d0affc8
SocialAdr <admin@socialadr.com>
2018-11-13 14:09:25 -0800

    Change http -> https

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 /* 
 	The page where you have the button/link for your users to grant your app access to their SocialAdr data 
 	
-	To find your App ID, go to http://socialadr.com/pg/apps/manage and click "Manage" for your app
+	To find your App ID, go to https://socialadr.com/pg/apps/manage and click "Manage" for your app
 */
 
 	$appId = ''; // Your App ID
-	$socialadr_url = "http://socialadr.com/pg/apps/details";
+	$socialadr_url = "https://socialadr.com/pg/apps/details";
 
 	echo "<a class=\"button\" href=\"$socialadr_url?id=$appId\">Authorize App</a>";
 	
